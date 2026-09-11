07c409198aa40f457eb9ea2f8ba908545b662fe9
Lafarmed <lafarmed2016@gmail.com>
2026-09-11 09:20:53 -0500

    Allow HTTPS SSL certificates for image loading from port 9191

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,8 +6,18 @@ import 'package:laboraya_app/app/app.dart';
 import 'package:laboraya_app/firebase_options.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
